281ba0829fa301bf5a380d3e4c0f3c6813c56740
WanderVafla <procenkovladik1@gmail.com>
2026-06-30 07:06:30 +0200

    feat: add value attributs

--- src/components/textarea.php
+++ src/components/textarea.php
@@ -1,11 +1,13 @@
 <?php
-function textarea(string $name = '', string $placeholder = '')
-{
+function textarea(
+    string $name = "",
+    string $placeholder = "",
+    string $value = "",
+) {
     ?>
 
     <div class="flex p-3 bg-blue-500 rounded-4xl focus-within:bg-red-500">
-        <textarea name="<?= $name ?>" class="w-2xl h-2xl text-white px-2 focus:outline-none resize-none field-sizing-content" placeholder="<?= $placeholder ?>">
-        </textarea>
+        <textarea name="<?= $name ?>" class="w-2xl h-2xl text-white px-2 focus:outline-none resize-none field-sizing-content" placeholder="<?= $placeholder ?>"><?= $value ?></textarea>
     </div>
 
 <?php
