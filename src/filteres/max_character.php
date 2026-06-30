03de24167ffef5918753fe15a0583267efa54f14
WanderVafla <procenkovladik1@gmail.com>
2026-06-30 14:06:53 +0200

    fix: replaced strlen() to mb_strlen(), for count the characters, not bytes

--- src/filteres/max_character.php
+++ src/filteres/max_character.php
@@ -3,7 +3,7 @@ function filter_max_characters(string $value, int $max_characters)
 {
     if (!filter_var($value, FILTER_CALLBACK, [
             "options" => function () use ($value, $max_characters) {
-                if (strlen($value) > $max_characters) {
+                if (mb_strlen($value) > $max_characters) {
                     return false;
                 } else {
                     return true;
