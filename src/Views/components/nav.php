ecb29b882475ecc64dc45fa35651c90a703d71c9
WanderVafla <procenkovladik1@gmail.com>
2026-07-02 08:26:24 +0200

    fix: changed routing button to news addresses

--- src/Views/components/nav.php
+++ src/Views/components/nav.php
@@ -1,6 +1,6 @@
 
 <nav class="flex justify-between items-center px-5 py-3">
-    <p class="text-5xl font-bold"><a href="index.php">PHP blog</a></p>
+    <p class="text-5xl font-bold"><a href="\">PHP blog</a></p>
     <span class="flex gap-3">
         <button type="button"><a href="login.php">Login</a></button>
         <button type="button"><a href="register_page.php">Reg in</a></button>
--- src/Views/components/post.php
+++ src/Views/components/post.php
@@ -1,11 +1,13 @@
 <?php
 function post(string $title, string $content, string $path)
 {
-    ?>
+    if (!file_exists($path)) {
+        $path = "/asset/notImage.png";
+    } ?>
     <div class='flex flex-col gap-2'>
         <img src="<?= $path ?>" alt="Post image" class="size-60 rounded-4xl" >
         <div class='flex flex-col px-2 py-1 gap-1'>
-            <p class='text-xl font-semibold'><a class="hover:text-red-500" href="single-detail-page.php"><?= $title ?></a></p>
+            <p class='text-xl font-semibold'><a class="hover:text-red-500" href="/post"><?= $title ?></a></p>
             <p class='text-wrap truncate w-60 h-30' >
                 <?= $content ?>
             </p>
